c88c1baf13ca4bbc114db2ab87b169ccb1fc833a
Sudeera Madushan <sudeeramadushan888@gmail.com>
2025-11-18 08:20:29 +0530

    Remove duplicate server.listen call in index.js to streamline server startup process.

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+// Run the server
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
